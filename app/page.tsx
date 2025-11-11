48c72403cc9de9c0e250d28bf3786d419ae57a17
Chen ShengZhong <sheng@ChendeMacBook-Air.local>
2025-11-11 12:33:08 +0800

    3rd commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
@@ -29,7 +28,7 @@ export default function Page() {
           </p>
 		  
           <Link
-            href="/login"
+            href="/dashboard/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
